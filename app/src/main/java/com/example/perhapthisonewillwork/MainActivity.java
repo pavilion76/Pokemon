ae50e07851dc9ca72a69e5e964328e8f64dbea27
Pavilion76 <paul_kratz@yahoo.com>
2019-10-22 00:27:56 -0600

    All thats left is to make the user input actually matter and clean up.

--- app/src/main/java/com/example/perhapthisonewillwork/MainActivity.java
+++ app/src/main/java/com/example/perhapthisonewillwork/MainActivity.java
@@ -2,17 +2,28 @@ package com.example.perhapthisonewillwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import okhttp3.Call;
@@ -25,12 +36,16 @@ import okhttp3.ResponseBody;
 
 
 public class MainActivity extends AppCompatActivity {
-    private ProgressBar progressBar;
     private String result;
     private URL url;
     private TextView resultText;
     private TextView pokemonName;
     myAsyncTask task = new myAsyncTask(this);
+    private String[] abilities;
+    private String height;
+    private String[] moves;
+    private ImageView spriteDisplay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
         Button getPokedex = (Button) findViewById(R.id.submit);
         pokemonName = (TextView) findViewById(R.id.pokemonName);
         resultText = (TextView) findViewById(R.id.resultText);
-        progressBar = findViewById(R.id.progress_bar);
+        spriteDisplay = (ImageView) findViewById(R.id.pokemonSprite);
+
 
         getPokedex.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,8 +89,6 @@ public class MainActivity extends AppCompatActivity {
             MainActivity activity = activityWeakReference.get();
             if (activity == null || activity.isFinishing())
                 return;
-
-            activity.progressBar.setVisibility(View.VISIBLE);
         }
         @Override
         protected void onProgressUpdate(Integer... values) {
@@ -83,8 +97,6 @@ public class MainActivity extends AppCompatActivity {
             MainActivity activity = activityWeakReference.get();
             if (activity == null || activity.isFinishing())
                 return;
-
-            progressBar.setProgress(values[0]);
         }
         @Override
         protected String doInBackground(URL... urls) {
@@ -97,11 +109,40 @@ public class MainActivity extends AppCompatActivity {
                         .build();
                 publishProgress(10);
 
-                Call call = client.newCall(request);
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull final Response response) throws IOException {
+                        if(response.isSuccessful()){
+                            final String myResponse = response.body().string();
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    jsonParse(myResponse);
+                                    try{
+                                        //Load the sprite
+                                        JSONObject obj = new JSONObject(myResponse);
+                                        JSONObject sprites = obj.getJSONObject("sprites");
+                                        String sprite = sprites.getString("front_default");
+                                        loadImageFromURL(sprite);
+                                    }catch (JSONException e){
+                                        resultText.append(e.getMessage());
+                                    }
+                                    resultText.append("\n\n");
+                                }
+                            });
+                        }
+                    }
+                });
+                /*Call call = client.newCall(request);
                 Response response = call.execute();
                 ResponseBody body = response.body();
-                String page=body.toString();
-            }catch (IOException e){
+                String page=body.toString();*/
+                throw new Exception("Why?");
+            }catch (Exception e){
                 e.getMessage();
             }finally {
                 //cleanup
@@ -114,10 +155,62 @@ public class MainActivity extends AppCompatActivity {
             MainActivity activity = activityWeakReference.get();
             if (activity == null || activity.isFinishing())
                 return;
+            activity.resultText.setText("Hey it actually made it to onPostExecute\n\n");
+        }
+    }
+    private void loadImageFromURL(String url){
+        Picasso.with(this).load(url).placeholder(R.mipmap.ic_launcher) //optional
+                .error(R.mipmap.ic_launcher)
+                .into(spriteDisplay,new com.squareup.picasso.Callback(){
+                    @Override
+                    public void onSuccess(){
 
-            activity.progressBar.setVisibility(View.INVISIBLE);
-            activity.resultText.setText("Hey it actually made it to onPostExecute");
-            activity.progressBar.setProgress(0);
+                    }
+                    @Override
+                    public void onError(){
+
+                    }
+                })
+;
+    }
+    private void jsonParse(String myResponse){
+        try {
+            //resultText.setText(myResponse);
+            //Abilities
+            result = myResponse;
+            JSONObject obj = new JSONObject(result);
+            JSONArray jsonArray = obj.getJSONArray("abilities");
+            resultText.append("Abilities\n");
+            for(int i=0; i<jsonArray.length();i++){
+                JSONObject ability = jsonArray.getJSONObject(i);
+                JSONObject ability2 = ability.getJSONObject("ability");
+                String nameOfAbility = ability2.getString("name");
+                resultText.append(nameOfAbility + "\n");
+            }
+            resultText.append("\n\n");
+
+
+            //Moves
+            jsonArray = obj.getJSONArray("moves");
+            resultText.append("Moves\n");
+            for(int i=0; i<jsonArray.length();i++){
+                JSONObject move = jsonArray.getJSONObject(i);
+                JSONObject move2 = move.getJSONObject("move");
+                String nameOfMove = move2.getString("name");
+                resultText.append(nameOfMove + "\n");
+            }
+            resultText.append("\n\n");
+
+
+
+
+                                        /*JSONObject jsonabilities = obj.getJSONObject("abilities");
+                                        abilities[0] = jsonabilities.getJSONObject("0").getString("name");
+                                        abilities[1] = jsonabilities.getJSONObject("1").getString("name");
+
+                                        resultText.setText(abilities[0]);*/
+        }catch (JSONException e){
+            resultText.setText(e.getMessage());
         }
     }
 }
